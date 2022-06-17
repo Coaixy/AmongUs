27db26bb5f75c9f1e639293b24069cbc4b194ef0
coaixy <coaixy@qq.com>
2022-06-17 19:02:50 +0800

    Changes

--- src/main/kotlin/com/github/coaixy/AmongUs/Commands/guiCommand.kt
+++ src/main/kotlin/com/github/coaixy/AmongUs/Commands/guiCommand.kt
@@ -1,2 +1,23 @@
 package com.github.coaixy.AmongUs.Commands
 
+import com.github.coaixy.AmongUs.Core.colored
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.command.subCommand
+
+val guiCommand = subCommand {
+    execute<ProxyCommandSender> { sender, context, argument ->
+        val player:ProxyCommandSender = sender
+        Bukkit.getPlayer(player.name)?.openInventory(inv)
+    }
+}
+val inv:Inventory
+get() {
+    val result = Bukkit.createInventory(null,54, colored("&eAmong Us"))
+    val testItem = ItemStack(Material.APPLE)
+    result.setItem(1,testItem)
+    return result
+}
--- src/main/kotlin/com/github/coaixy/AmongUs/Core/Command.kt
+++ src/main/kotlin/com/github/coaixy/AmongUs/Core/Command.kt
@@ -1,5 +1,6 @@
 package com.github.coaixy.AmongUs.Core
 
+import com.github.coaixy.AmongUs.Commands.guiCommand
 import com.github.coaixy.AmongUs.Commands.helpCommand
 import taboolib.common.platform.command.CommandBody
 import taboolib.common.platform.command.CommandHeader
@@ -18,4 +19,10 @@ object Command {
         permission = "AmongUs.user.help"
     )
     val help = helpCommand
+    @CommandBody(
+        optional = true,
+        permissionDefault = PermissionDefault.TRUE,
+        permission = "AmongUs.user.gui"
+    )
+    val gui = guiCommand
 }
